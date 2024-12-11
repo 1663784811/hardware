c08abb20a3115f2d1d627dcc618234b69ada15e8
why <1663784811@qq.com>
2024-12-12 00:06:24 +0800

    init

---
+++ stc/stc15w408as/05接收pwm中断/config.h
@@ -0,0 +1,99 @@
+
+/*------------------------------------------------------------------*/
+/* --- STC MCU International Limited -------------------------------*/
+/* --- STC 1T Series MCU RC Demo -----------------------------------*/
+/* --- Mobile: (86)13922805190 -------------------------------------*/
+/* --- Fax: 86-0513-55012956,55012947,55012969 ---------------------*/
+/* --- Tel: 86-0513-55012928,55012929,55012966 ---------------------*/
+/* --- Web: www.GXWMCU.com -----------------------------------------*/
+/* --- QQ:  800003751 ----------------------------------------------*/
+/* If you want to use the program or the program referenced in the  */
+/* article, please specify in which data and procedures from STC    */
+/*------------------------------------------------------------------*/
+
+
+#ifndef		__CONFIG_H
+#define		__CONFIG_H
+
+
+/*********************************************************/
+
+//#define MAIN_Fosc		22118400L	//定义主时钟
+//#define MAIN_Fosc		12000000L	//定义主时钟
+#define MAIN_Fosc		11059200L	//定义主时钟
+//#define MAIN_Fosc		 5529600L	//定义主时钟
+//#define MAIN_Fosc		24000000L	//定义主时钟
+
+
+/*********************************************************/
+#include <STC15.H>
+
+
+sfr	TH2  = 0xD6;
+sfr	TL2  = 0xD7;
+sfr PCON2   = 0x97;
+
+
+
+
+/**********************************************************/
+typedef 	unsigned char	u8;
+typedef 	unsigned int	u16;
+typedef 	unsigned long	u32;
+
+
+
+/***************************************   interrupt vector ***************************************/
+#define		INT0_VECTOR		0
+#define		TIMER0_VECTOR	1
+#define		INT1_VECTOR		2
+#define		TIMER1_VECTOR	3
+#define		UART1_VECTOR	4
+#define		ADC_VECTOR		5
+#define		LVD_VECTOR		6
+#define		PCA_VECTOR		7
+#define		UART2_VECTOR	8
+#define		SPI_VECTOR		9
+#define		INT2_VECTOR		10
+#define		INT3_VECTOR		11
+#define		TIMER2_VECTOR	12
+#define		INT4_VECTOR		16
+#define		UART3_VECTOR	17
+#define		UART4_VECTOR	18
+#define		TIMER3_VECTOR	19
+#define		TIMER4_VECTOR	20
+#define		CMP_VECTOR		21
+
+
+#define	TRUE	1
+#define	FALSE	0
+
+//=============================================================
+
+//========================================
+
+#define	PolityLow			0	//低优先级中断
+#define	PolityHigh			1	//高优先级中断
+
+//========================================
+#define		ENABLE		1
+#define		DISABLE		0
+
+
+
+#define NOP(N)  NOP##N()
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
+
---
+++ stc/stc15w408as/05接收pwm中断/main.c
@@ -0,0 +1,157 @@
+#include	"config.h"
+#include  "Exti.h"
+#include	"USART1.h"
+#include	"delay.h"
+#include  "timer.h"
+#include  "GPIO.h"
+/*************	功能说明	**************
+
+
+******************************************/
+
+
+
+/*************	本地常量声明	**************/
+
+
+/*************	本地变量声明	**************/
+
+
+
+/*************	本地函数声明	**************/
+void	EXTI_config(void);
+//串口1初始化函数
+void	UART_config(void);
+// 
+void	Timer_config(void);
+
+/*************  外部函数和变量声明 *****************/
+
+
+/**********************************************/
+void main(void)
+{
+	EXTI_config();
+	UART_config();
+	Timer_config();
+	EA = 1;             // 允许中断
+	PrintString1("init success");
+	// ==========================================
+	
+	while (1)
+	{
+		delay_ms(1000);
+		PrintString1("test");
+		
+	}
+}
+
+/**
+*   通过定时器输入捕获功能来检测 PWM 信号
+*/
+void External0_ISR(void) interrupt INT0_VECTOR {
+		PrintString1("===");	
+    if (IT0) {
+			PrintString1("up");
+			// 切换为下降沿触发
+			IT0 = 0;
+    } else {
+			PrintString1("down");			
+			// 切换为上升沿触发
+			IT0 = 1;
+    }
+}
+
+
+
+
+/********************* Timer0中断函数  产生pwm波 ********************/
+void timer0_int (void) interrupt TIMER0_VECTOR
+{
+  P11 = ~P11;
+			delay_ms(1000);
+}
+
+
+/********************* Timer1中断函数   ********************/
+void timer1_int (void) interrupt TIMER1_VECTOR
+{
+  P10 = ~P10;
+			delay_ms(1000);
+}
+
+
+
+
+
+/************************************     串口1初始化函数     ***************************************/
+void	UART_config(void)
+{
+	COMx_InitDefine		COMx_InitStructure;					//结构定义
+	COMx_InitStructure.UART_Mode      = UART_8bit_BRTx;		//模式,       UART_ShiftRight,UART_8bit_BRTx,UART_9bit,UART_9bit_BRTx
+	COMx_InitStructure.UART_BRT_Use   = BRT_Timer2;			//使用波特率,   BRT_Timer1, BRT_Timer2 (注意: 串口2固定使用BRT_Timer2)
+	COMx_InitStructure.UART_BaudRate  = 115200ul;			//波特率, 一般 110 ~ 115200
+	COMx_InitStructure.UART_RxEnable  = ENABLE;				//接收允许,   ENABLE或DISABLE
+	COMx_InitStructure.BaudRateDouble = DISABLE;			//波特率加倍, ENABLE或DISABLE
+	COMx_InitStructure.UART_Interrupt = ENABLE;				//中断允许,   ENABLE或DISABLE
+	COMx_InitStructure.UART_Polity    = PolityLow;			//中断优先级, PolityLow,PolityHigh
+	COMx_InitStructure.UART_P_SW      = UART1_SW_P30_P31;	//切换端口,   UART1_SW_P30_P31,UART1_SW_P36_P37,UART1_SW_P16_P17(必须使用内部时钟)
+	COMx_InitStructure.UART_RXD_TXD_Short = DISABLE;		//内部短路RXD与TXD, 做中继, ENABLE,DISABLE
+	USART_Configuration(USART1, &COMx_InitStructure);		//初始化串口1 USART1,USART2
+	PrintString1("STC15F2K60S2 UART1 Test Prgramme!\r\n");	//SUART1发送一个字符串
+}
+
+
+/*************  外部函数和变量声明 *****************/
+
+void	EXTI_config(void)
+{
+	EXTI_InitTypeDef	EXTI_InitStructure;					//结构定义
+
+	EXTI_InitStructure.EXTI_Mode      = EXT_MODE_RiseFall;	//中断模式,  	EXT_MODE_RiseFall, EXT_MODE_Fall
+	EXTI_InitStructure.EXTI_Polity    = PolityHigh;			//中断优先级,   PolityLow,PolityHigh
+	EXTI_InitStructure.EXTI_Interrupt = ENABLE;				//中断允许,     ENABLE或DISABLE
+	Ext_Inilize(EXT_INT0,&EXTI_InitStructure);				//初始化INT0	EXT_INT0,EXT_INT1,EXT_INT2,EXT_INT3,EXT_INT4
+}
+
+
+
+/*************************************        定时器配置      **************************************/
+void	Timer_config(void)
+{
+	TIM_InitTypeDef		TIM_InitStructure;					//结构定义
+	TIM_InitStructure.TIM_Mode      = TIM_16BitAutoReload;	//指定工作模式,   TIM_16BitAutoReload,TIM_16Bit,TIM_8BitAutoReload,TIM_16BitAutoReloadNoMask
+	TIM_InitStructure.TIM_Polity    = PolityLow;			//指定中断优先级, PolityHigh,PolityLow
+	TIM_InitStructure.TIM_Interrupt = ENABLE;				//中断是否允许,   ENABLE或DISABLE
+	TIM_InitStructure.TIM_ClkSource = TIM_CLOCK_1T;			//指定时钟源,     TIM_CLOCK_1T,TIM_CLOCK_12T,TIM_CLOCK_Ext
+	TIM_InitStructure.TIM_ClkOut    = ENABLE;				//是否输出高速脉冲, ENABLE或DISABLE
+	TIM_InitStructure.TIM_Value     = 32767;		//初值,
+	TIM_InitStructure.TIM_Run       = ENABLE;				//是否初始化后启动定时器, ENABLE或DISABLE
+	Timer_Inilize(Timer0,&TIM_InitStructure);				//初始化Timer0	  Timer0,Timer1,Timer2
+
+	
+	
+	
+	
+	
+	TIM_InitStructure.TIM_Mode      = TIM_16BitAutoReload;
+	TIM_InitStructure.TIM_Polity    = PolityLow;
+	TIM_InitStructure.TIM_Interrupt = ENABLE;
+	TIM_InitStructure.TIM_ClkSource = TIM_CLOCK_1T;
+	TIM_InitStructure.TIM_ClkOut    = ENABLE;
+	TIM_InitStructure.TIM_Value     = 1;
+	TIM_InitStructure.TIM_Run       = ENABLE;
+	Timer_Inilize(Timer1,&TIM_InitStructure);
+
+}
+
+/************************************       IO口配置       *****************************************/
+void	GPIO_config(void)
+{
+	GPIO_InitTypeDef	GPIO_InitStructure;				//结构定义
+	GPIO_InitStructure.Pin  = GPIO_Pin_0 | GPIO_Pin_1;	//指定要初始化的IO, GPIO_Pin_0 ~ GPIO_Pin_7, 或操作
+	GPIO_InitStructure.Mode = GPIO_OUT_PP;				//指定IO的输入或输出方式,GPIO_PullUp,GPIO_HighZ,GPIO_OUT_OD,GPIO_OUT_PP
+	GPIO_Inilize(GPIO_P1,&GPIO_InitStructure);			//初始化
+	
+}
+
